57d741e57b168a49c89e453a1ab9cc2ba64b4379
danbendell <danbendell@gmail.com>
2015-12-12 16:14:40 +0000

    Basics for creating and displaying a Windows window. With exit functionality and key response

---
+++ DirectX11Project/DirectX11Project/graphicsclass.h
@@ -0,0 +1,31 @@
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+#include <windows.h>
+
+// GLOBALS
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+class GraphicsClass
+{
+	public:
+		GraphicsClass();
+		GraphicsClass(const GraphicsClass&);
+		~GraphicsClass();
+
+		bool Initialize(int, int, HWND);
+		void Shutdown();
+		bool Frame();
+
+	private:
+		bool Render();
+
+	private:
+
+};
+
+#endif
+
